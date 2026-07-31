66e82cf2da08d39d2f8fb851f7b089b3a12602cf
medomohamed22 <123499261+medomohamed22@users.noreply.github.com>
2026-07-31 22:54:09 +0300

    Delete api directory

--- api/version.js
+++
@@ -1,6 +0,0 @@
-import {allowMethods,json} from './_lib.js';
-export default function handler(req,res){
-  if(!allowMethods(req,res,['GET']))return;
-  const version=process.env.VERCEL_GIT_COMMIT_SHA||process.env.VERCEL_DEPLOYMENT_ID||process.env.APP_VERSION||'local-development';
-  return json(res,200,{version});
-}
